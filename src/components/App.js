66d98c914f10a70c6b363941d370f098615a79bc
jakestucky <jakestucky@gmail.com>
2021-07-15 16:31:01 -0500

    added in async spinner and cond rendering

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
 import ManageCoursePage from "./courses/ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
         <Route path="/courses" component={CoursesPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
         <Route path="/course" component={ManageCoursePage} />
+        <Route component={PageNotFound} />
       </Switch>
+      <ToastContainer autoClose={3000} hideProgressBar />
     </div>
   );
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
+import apiCallsInProgress from "./apiStatusReducer";
 import authors from "./authorReducer";
 
 const rootReducer = combineReducers({
   courses,
   authors,
+  apiCallsInProgress,
 });
 
 export default rootReducer;
