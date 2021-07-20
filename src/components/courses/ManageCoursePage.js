e1929d68584d119c190dd10ec0cb57ee238254f5
jakestucky <jakestucky@gmail.com>
2021-07-20 10:17:17 -0500

    added testing for redux

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -8,7 +8,7 @@ import { newCourse } from "../../../tools/mockData";
 import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
 
-function ManageCoursePage({
+export function ManageCoursePage({
   courses,
   authors,
   loadAuthors,
@@ -23,7 +23,7 @@ function ManageCoursePage({
 
   useEffect(() => {
     if (courses.length === 0) {
-      loadCourses().catch((error) => {
+      loadCourses().catch(error => {
         alert("Loading courses failed" + error);
       });
     } else {
@@ -31,7 +31,7 @@ function ManageCoursePage({
     }
 
     if (authors.length === 0) {
-      loadAuthors().catch((error) => {
+      loadAuthors().catch(error => {
         alert("Loading authors failed" + error);
       });
     }
@@ -39,11 +39,12 @@ function ManageCoursePage({
 
   function handleChange(event) {
     const { name, value } = event.target;
-    setCourse((prevCourse) => ({
+    setCourse(prevCourse => ({
       ...prevCourse,
-      [name]: name === "authorId" ? parseInt(value, 10) : value,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
     }));
   }
+
   function formIsValid() {
     const { title, authorId, category } = course;
     const errors = {};
@@ -63,10 +64,10 @@ function ManageCoursePage({
     setSaving(true);
     saveCourse(course)
       .then(() => {
-        toast.success("Course saved!");
+        toast.success("Course saved.");
         history.push("/courses");
       })
-      .catch((error) => {
+      .catch(error => {
         setSaving(false);
         setErrors({ onSave: error.message });
       });
@@ -93,11 +94,11 @@ ManageCoursePage.propTypes = {
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   saveCourse: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 
 export function getCourseBySlug(courses, slug) {
-  return courses.find((course) => course.slug === slug) || null;
+  return courses.find(course => course.slug === slug) || null;
 }
 
 function mapStateToProps(state, ownProps) {
@@ -109,14 +110,17 @@ function mapStateToProps(state, ownProps) {
   return {
     course,
     courses: state.courses,
-    authors: state.authors,
+    authors: state.authors
   };
 }
 
 const mapDispatchToProps = {
   loadCourses,
   loadAuthors,
-  saveCourse,
+  saveCourse
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
